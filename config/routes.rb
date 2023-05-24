4251141609d78b51643dddc789a7e21a7748bb53
aubinleyoung <aubinsimpeze01@gmail.com>
2023-05-24 11:55:15 +0200

    Add authentification for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  
+  resources :users, only: [:index, :show]
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'users#index', as: :unauthenticated_root
+    end
+  end
 end
